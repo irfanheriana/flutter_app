a787c29217dccedb1cc5d69d2f356eb33e5f1a8b
Irfan Heriana <irfanheriana17@gmail.com>
2022-04-09 08:50:22 +0700

    commit looping1

--- test/looping.dart
+++ test/looping.dart
@@ -1,14 +1,5 @@
 import 'dart:io';
 
-// void main() {
-//   for (int i = 1; i <= 5; i++) {
-//     for (int j = 1; j <= i; j++) {
-//       stdout.write("*");
-//     }
-//     stdout.write("\n");
-//   }
-// }
-
 void main() {
   for (int i = 1; i <= 5; i++) {
     for (int j = 1; j <= i; j++) {
